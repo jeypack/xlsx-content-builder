65ec83cf6dd1eda093f16e8be1d1c6ba1b54f9c7
joerg.pfeifer@egplusww.com <joerg.pfeifer@egplusww.com>
2022-07-18 22:40:39 +0200

    Extract gulp-config.js out of gulp-xlsx-builder.js

--- gulpfile.js/gulp-xlsx-builder.js
+++ gulpfile.js/gulp-xlsx-builder.js
@@ -27,326 +27,22 @@ const { v4: uuidv4 } = require("uuid");
 const XLSX = require("xlsx");
 const zipper = require("./gulp-zipper");
 const {
-  XLSX_TYPE_ENUM,
   getTemplateValues,
   setTemplate,
   setTemplateValues,
 } = require("./gulp-xlsx-helper.js");
-//const { type } = require("os");
-const { config } = require("./config.js");
-
-const TPL_ENUM = {
-  STD: "NAME",
-  LANG: "LANG",
-  VERSION: "VERSION",
-  LANG_VERSION: "LANG_VERSION",
-};
-/* 
-//config.DEV_FOLDER
-let config = {
-  UID: 0,
-  DEVELOPMENT: true,
-  HAS_FOLDER_TO_ZIP: false,
-  //PATH_INCLUDES_SASS: ['bower_components/juiced/sass/'],
-  HTDOCS_PATH: "/Applications/MAMP/htdocs/",
-  SRC_PATH: "./src/xlsx-template/",
-  SRC_VENDOR: "./src/vendor/",
-  DEV_FOLDER: "./_temp/",
-  BUILD_FOLDER: "./_build/",
-  CLIENT: "P&G",
-  // PREFIX_BRAND_PRODUCT_TYPE_LANGUAGE_VERSION_SIZE_CLIENT_VERSION_DATE
-  //e.g. HTML5_OralB_Genesis5_KIDS-Lightyear_HU_V1_1195xAUTO_V01_220626
-  TPL_NAMES: {
-    BRAND: ["OralB", "OralB"],
-    PRODUCT: ["Genesis5", "Genesis5"],
-    TYPE: ["KIDS-Lightyear", "KIDS-Lightyear"],
-    LANGUAGE: [
-      ["CZ", "HU", "PL", "SK"],
-      ["CZ", "HU", "PL", "SK"],
-    ],
-    //every language may have more than 1 version
-    //here we keep version length for every language entry
-    //e.g. TPL_NAMES.VERSION[CURRENT_LANGUAGE] = 1 means one version ...
-    VERSION: [
-      [1, 1, 1, 1],
-      [1, 1, 1, 1],
-    ],
-    FLEX_COLS: [
-      [3, 3, 3, 3],
-      [3, 3, 3, 3],
-    ],
-    CLIENT_VERSION: ["V01", "V01"],
-    BODY_CLASS: [
-      ["mobile", "mobile", "mobile", "mobile"],
-      ["mobile", "mobile", "mobile", "mobile"],
-    ],
-    XLSX_PARSER: [
-      XLSX_TYPE_ENUM.ORAL_B_KIDS_1_4_4_1_FLEX_3,
-      XLSX_TYPE_ENUM.ORAL_B_KIDS_1_4_4_1_FLEX_3,
-    ],
-    DATE: ["220626", "220626"],
-    SIZE: "1195xAUTO",
-    PREFIX: "HTML5",
-    SUFFIX: "",
-  },
-  //
-  CURRENT: 0,
-  CURRENT_LANGUAGE: 0,
-  CURRENT_VERSION: 0,
-  CURRENT_TPL_VERSION: "LANG",
-  destination: "./_temp/",
-};
- */
-// HELPER FUNCTIONS GULP
-
-const directoryContains = (path, doneFn, errorFn) => {
-  fs.readdir(path, function (err, files) {
-    if (err) {
-      if (errorFn && typeof errorFn === "function") {
-        errorFn(err);
-      }
-    } else {
-      if (doneFn && typeof doneFn === "function") {
-        doneFn({ files: files });
-      }
-    }
-  });
-};
-
-const getTplNameFunction = () => {
-  switch (config.CURRENT_TPL_VERSION) {
-    case TPL_ENUM.LANG:
-      //images and scss for every language
-      return getTplLangName;
-    case TPL_ENUM.VERSION:
-      //images and scss for every version
-      return getTplVersionName;
-    case TPL_ENUM.LANG_VERSION:
-      //images and scss for every language and version
-      return getTplLangVersionName;
-    default:
-      //same images and scss for all pages
-      return getTplName;
-  }
-};
-// BRAND_PRODUCT_TYPE_DATE
-const getTplName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const name =
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index] +
-    "_" +
-    tplNames.DATE[index];
-  console.log("getTplName", "NAME:", name);
-  return name;
-};
-// BRAND_PRODUCT_TYPE_LANGUAGE_DATE
-const getTplLangName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  const name =
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index] +
-    "_" +
-    tplNames.LANGUAGE[index][languageIndex] +
-    "_" +
-    tplNames.DATE[index];
-  console.log("getTplLangName", "NAME:", name);
-  return name;
-};
-// BRAND_PRODUCT_TYPE_VERSION_DATE
-const getTplVersionName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  const versionIndex = config.CURRENT_VERSION;
-  const name =
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index] +
-    "_" +
-    (versionIndex + 1) +
-    "_" +
-    tplNames.DATE[index];
-  console.log("getTplVersionName", "NAME:", name);
-  return name;
-};
-// BRAND_PRODUCT_TYPE_LANGUAGE_VERSION_DATE
-const getTplLangVersionName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  const versionIndex = config.CURRENT_VERSION;
-  const name =
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index] +
-    "_" +
-    tplNames.LANGUAGE[index][languageIndex] +
-    "_" +
-    (versionIndex + 1) +
-    "_" +
-    tplNames.DATE[index];
-  console.log("getTplLangVersionName", "NAME:", name);
-  return name;
-};
-//BRAND_PRODUCT_TYPE_DATE
-const getZipName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const name =
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index];
-  console.log("getZipName", "NAME:", name);
-  return name;
-};
-//BRAND_PRODUCT_TYPE_LANGUAGE_VERSION_DATE
-const getXLSXName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  const versionIndex = config.CURRENT_VERSION;
-  const name =
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index] +
-    "_" +
-    tplNames.LANGUAGE[index][languageIndex] +
-    "_" +
-    (versionIndex + 1) +
-    "_" +
-    tplNames.DATE[index];
-  //console.log("getXLSXName", "NAME:", name);
-  return name;
-};
-//PREFIX_BRAND_PRODUCT_TYPE_LANGUAGE_VERSION_SIZE_CLIENT_VERSION_DATE
-const getOutputName = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  const versionIndex = config.CURRENT_VERSION;
-  const name =
-    tplNames.PREFIX +
-    "_" +
-    tplNames.BRAND[index] +
-    "_" +
-    tplNames.PRODUCT[index] +
-    "_" +
-    tplNames.TYPE[index] +
-    "_" +
-    tplNames.LANGUAGE[index][languageIndex] +
-    "_" +
-    (versionIndex + 1) +
-    "_" +
-    tplNames.SIZE +
-    tplNames.CLIENT_VERSION[index] +
-    "_" +
-    tplNames.DATE[index];
-  console.log("getOutputName", "NAME:", name);
-  return name;
-};
-const getLanguage = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  return tplNames.LANGUAGE[index][languageIndex];
-};
-const geBodyClass = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  return tplNames.BODY_CLASS[index][languageIndex];
-};
-const geXlsxParser = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  return tplNames.XLSX_PARSER[index];
-};
-const getLanguageVersion = () => {
-  return config.CURRENT_VERSION + 1;
-};
-const getFLexCols = () => {
-  const tplNames = config.TPL_NAMES;
-  const index = config.CURRENT;
-  const languageIndex = config.CURRENT_LANGUAGE;
-  return tplNames.FLEX_COLS[index][languageIndex];
-};
-const getVersion = () => {
-  return "STD TPL V2.0.0 | 26.06.2022 | " + new Date().toDateString();
-};
-const extendTemplateVars = (obj) => {
-  if (typeof obj !== "object") {
-    obj = {};
-  }
-  //get template name function via enum
-  const getTplNameFunc = getTplNameFunction();
-  obj.version = getVersion();
-  obj.title = getXLSXName();
-  obj.template = getTplNameFunc();
-  obj.language = getLanguage();
-  obj.languageVersion = getLanguageVersion();
-  obj.bodyClass = geBodyClass();
-  obj.jsCssName = config.DEVELOPMENT
-    ? "index.min"
-    : "index." + config.UID + ".min";
-  obj.flexSliderCols = getFLexCols();
-  console.log("extendTemplateVars", "obj:", obj);
-  return obj;
-};
-/**
- * Gulp nex index iterator
- */
-const nextIndex = () => {
-  const tpls = config.TPL_NAMES;
-  const length = tpls.BRAND.length;
-  const languageLength = tpls.LANGUAGE[config.CURRENT].length;
-  const versionLength = tpls.VERSION[config.CURRENT][config.CURRENT_LANGUAGE];
-  const nextIndex = config.CURRENT + 1;
-  const nextLanguageIndex = config.CURRENT_LANGUAGE + 1;
-  const nextVersionIndex = config.CURRENT_VERSION + 1;
-  if (nextVersionIndex < versionLength) {
-    config.CURRENT_VERSION = nextVersionIndex;
-  } else {
-    config.CURRENT_VERSION = 0;
-    if (nextLanguageIndex < languageLength) {
-      config.CURRENT_LANGUAGE = nextLanguageIndex;
-    } else {
-      config.CURRENT_LANGUAGE = 0;
-      if (nextIndex < length) {
-        config.CURRENT = nextIndex;
-      } else {
-        return false;
-      }
-    }
-  }
-  console.log(
-    "nextIndex",
-    "CURRENT:",
-    config.CURRENT,
-    "CURRENT_LANGUAGE:",
-    config.CURRENT_LANGUAGE,
-    "CURRENT_VERSION:",
-    config.CURRENT_VERSION
-  );
-  return true;
-};
+const {
+  config,
+  directoryContains,
+  getTplNameFunction,
+  getTplName,
+  getZipName,
+  getXLSXName,
+  getOutputName,
+  geXlsxParser,
+  extendTemplateVars,
+  nextIndex,
+} = require("./gulp-config.js");
 
 // GULP ENABLED
 
@@ -637,26 +333,47 @@ const watchDirectory = (cb) => {
 
 const setConfigHasFolderToZip = (cb) => {
   const folder = config.DEVELOPMENT ? config.DEV_FOLDER : config.BUILD_FOLDER;
-  directoryContains(folder, (resp) => {
-    const length = resp.files.length;
-    /* for (let i = 0; i < length; i++) {
+  directoryContains(
+    folder,
+    (resp) => {
+      const length = resp.files.length;
+      /* for (let i = 0; i < length; i++) {
       console.log("zip", "i:", i, "files:", resp.files[i]);
     } */
-    config.HAS_FOLDER_TO_ZIP = length > 1;
-    console.log("zip", "folder:", folder, "config.HAS_FOLDER_TO_ZIP:", config.HAS_FOLDER_TO_ZIP);
-    cb();
-  }, (error) => {
-    config.HAS_FOLDER_TO_ZIP = false;
-    console.log("zip", "folder:", folder, "error:", error.substr(0, 20), "...");
-    cb();
-  });
+      config.HAS_FOLDER_TO_ZIP = length > 1;
+      console.log(
+        "zip",
+        "folder:",
+        folder,
+        "config.HAS_FOLDER_TO_ZIP:",
+        config.HAS_FOLDER_TO_ZIP
+      );
+      cb();
+    },
+    (error) => {
+      config.HAS_FOLDER_TO_ZIP = false;
+      console.log(
+        "zip",
+        "folder:",
+        folder,
+        "error:",
+        error.substr(0, 20),
+        "..."
+      );
+      cb();
+    }
+  );
 };
 
 const zip = (cb) => {
   const folder = config.DEVELOPMENT ? config.DEV_FOLDER : config.BUILD_FOLDER;
   const name = getZipName();
   console.log("zip", "folder:", folder);
-  const stream = src([folder + "*", "!" + folder + "zip", "!" + folder + "*.*"]).pipe(
+  const stream = src([
+    folder + "*",
+    "!" + folder + "zip",
+    "!" + folder + "*.*",
+  ]).pipe(
     zipper({ destination: folder + "zip/", name: name }, (evt) => {
       console.log(
         "zip :",
@@ -679,6 +396,8 @@ const zip = (cb) => {
   }
   cb();
 };
+
+// HELPERS
 
 const buildTemplate = series(
   setDestination,
@@ -759,7 +478,14 @@ exports.clean = series(
   cleanDirectory
 );
 
-exports.zip = series(enableDevelopment, setConfigHasFolderToZip, zip, enableProduction, setConfigHasFolderToZip, zip);
+exports.zip = series(
+  enableDevelopment,
+  setConfigHasFolderToZip,
+  zip,
+  enableProduction,
+  setConfigHasFolderToZip,
+  zip
+);
 
 //+++ R.I.P. +++
 //npm init react-app ./my-react-app
